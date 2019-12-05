927a73bc11bba0684052cb6a17bc8bb80cf22f3f
Maija <maijaaaw@gmail.com>
2019-12-05 14:41:30 +0200

    Use better component for the side menu

--- front/src/components/sidemenu/index.js
+++ front/src/components/sidemenu/index.js
@@ -1,47 +1,23 @@
 import React, { Component } from "react";
 import { Icon, Menu, Segment, Sidebar } from "semantic-ui-react";
 
-/*
 export default class SideMenu extends Component {
   render() {
     return (
-      <div>
-        <LogIn />
-        <MyProfile />
-      </div>
-    );
-  }
-}
-*/
-
-export default class SideMenu extends Component {
-  render() {
-    return (
-      // Toi "as={Segment}" aiheuttaa sen haamulaatikon.
-      <Sidebar.Pushable /*as={Segment}*/>
-        <Sidebar
-          as={Menu}
-          animation="overlay"
-          icon="labeled"
-          inverted
-          vertical
-          visible
-          width="thin"
-        >
-          <Menu.Item as="a">
-            <Icon name="sign in" />
-            Log in
-          </Menu.Item>
-          <Menu.Item as="a">
-            <Icon name="user circle" />
-            My blog
-          </Menu.Item>
-          <Menu.Item as="a">
-            <Icon name="th" />
-            Feed
-          </Menu.Item>
-        </Sidebar>
-      </Sidebar.Pushable>
+      <Menu vertical fluid>
+        <Menu.Item as="a">
+          <Icon name="sign in" />
+          Log in
+        </Menu.Item>
+        <Menu.Item as="a">
+          <Icon name="user circle" />
+          My blog
+        </Menu.Item>
+        <Menu.Item as="a">
+          <Icon name="th" />
+          Feed
+        </Menu.Item>
+      </Menu>
     );
   }
 }
