39b54d34063fcd7248ee87bea2dac50a21c6ef15
Maija <maijaaaw@gmail.com>
2019-12-10 17:30:24 +0200

    -

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -2,7 +2,6 @@ var express = require("express");
 var { Post } = require("../db");
 var router = express.Router();
 
-/* GET home page. */
 router.get("/", async function(req, res, next) {
   const posts = await Post.find().exec();
 
