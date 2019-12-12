6b7ae32f89254d1ba41ada943b9f39253c580eba
Maija <maijaaaw@gmail.com>
2019-12-12 16:45:23 +0200

    Add char counter and button disable if 0 chars

--- front/src/components/CreatePost.js
+++ front/src/components/CreatePost.js
@@ -3,18 +3,22 @@ import { TextArea, Button, Grid, Form } from "semantic-ui-react";
 
 class CreatePost extends Component {
   state = {
-    textContent: ""
+    textContent: "",
+    charsLeft: 120,
+    buttonState: true
   };
 
   saveNewPost() {
-    const { onNewPost, currentUser } = this.props;
-    onNewPost({
-      // TODO: Add user controlling kikkare, tyyliin propseihin tms.
-      name: currentUser,
-      textContent: this.state.textContent,
-      date: new Date()
-    });
-    this.setState({ textContent: "" });
+    if (this.state.textContent.length > 0) {
+      const { onNewPost, currentUser } = this.props;
+      onNewPost({
+        // TODO: Add user controlling kikkare, tyyliin propseihin tms.
+        name: currentUser,
+        textContent: this.state.textContent,
+        date: new Date()
+      });
+      this.setState({ textContent: "", charsLeft: 120, buttonState: true });
+    }
   }
   render() {
     return (
@@ -25,19 +29,33 @@ class CreatePost extends Component {
             rows={3}
             placeholder="Write a post..."
             onChange={(event, data) => {
-              this.setState({ textContent: data.value });
-              //console.log("state vaihdettu");
+              if (data.value.length <= 120) {
+                this.setState({
+                  textContent: data.value,
+                  charsLeft: 120 - data.value.length
+                });
+                //console.log("state vaihdettu");
+              }
+              if (data.value.length > 0) {
+                this.setState({ buttonState: false });
+              } else {
+                this.setState({ buttonState: true });
+              }
             }}
             value={this.state.textContent}
           />
         </Form>
         <Grid>
-          <Grid.Row textAlign="right">
-            <Grid.Column>
+          <Grid.Row>
+            <Grid.Column width={12}>
+              <p>Characters used: {120 - this.state.charsLeft}/120</p>
+            </Grid.Column>
+            <Grid.Column width={4} textAlign="right">
               <Button
                 primary
                 style={{ clear: "both" }}
                 onClick={() => this.saveNewPost()}
+                disabled={this.state.buttonState}
               >
                 Post
               </Button>
